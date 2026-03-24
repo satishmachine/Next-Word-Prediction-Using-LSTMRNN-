c8bf4d14b18ad9a3bf28f01bee5b72959177cde2
satishmachine <satishkumar8055@gmail.com>
2026-03-24 22:10:42 +0530

    updated app.py file

--- app.py
+++ app.py
@@ -24,14 +24,16 @@ except ModuleNotFoundError as e:
 def predict_next_word(model, tokenizer, text, max_sequence_len):
     token_list = tokenizer.texts_to_sequences([text])[0]
     if len(token_list) >= max_sequence_len:
-        token_list = token_list[-(max_sequence_len-1):]  # Ensure the sequence length matches max_sequence_len-1
+        token_list = token_list[-(max_sequence_len-1):]
     token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
     predicted = model.predict(token_list, verbose=0)
-    predicted_word_index = np.argmax(predicted, axis=1)
+    predicted_word_index = np.argmax(predicted, axis=1)[0]
+    
+    # Lookup word from tokenizer vocabulary
     for word, index in tokenizer.word_index.items():
         if index == predicted_word_index:
             return word
-    return None
+    return "Unknown"
 
 # streamlit app
 st.title("Next Word Prediction With LSTM And Early Stopping")
